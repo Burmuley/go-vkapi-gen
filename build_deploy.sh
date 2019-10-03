ded555bcf862e1fcc0411e40585ddb78937df7ff
konstantin_vasilev <konstantin_vasilev@epam.com>
2019-10-03 15:40:45 -0700

    Fixed build script; Moved git setup to build script from before_script

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+
+eval $(ssh-agent -s)
+echo "$GOVKAPI_SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
+
 # build GO VKAPI Generator
 go build -o go-vkapi-gen
 
 # run generator and check exit code
-if [[ "$(go-vkapi-gen)" -ne 0 ]]; then
+if [[ "$(./go-vkapi-gen)" -ne 0 ]]; then
   echo "ERROR RUNNING GO VKAPI GENERATOR"
   exit 1
 fi
 
 # clone GO VKAPI repo
-git "$GOVKAPI_SSH_REPO_URL"
+git clone "$GOVKAPI_SSH_REPO_URL"
 cd "$GOVKAPI_REPO_DIR" || exit 1
+
+git config --global user.email "cicd-go-vkapi-gen@gitlab.com"
+git config --global user.name "CIDI GO VK API Generator"
 
 # create a new branch named (HOW??)
 br_name=$(date +"generated-%m-%d-%Y-%H-%M-%S")
